18573478d13dffe280bf389e8b858ffeeb8bec27
MrDen25 <Kreativchik30@gmail.com>
2026-01-02 09:49:52 +0200

    Add files via upload

---
+++ 019_rem-em-vh-vw/js/script.js
@@ -0,0 +1,8 @@
+let burgerIcon = document.querySelector(".header__icon")
+let burgerMenu = document.querySelector(".menu-header")
+let logo = document.querySelector(".header__logo")
+burgerIcon.addEventListener("click", () => {
+    burgerMenu.classList.toggle("menu-header__active")
+    burgerIcon.classList.toggle('header__icon-active')
+    logo.classList.toggle("header__logo-active")
+})
